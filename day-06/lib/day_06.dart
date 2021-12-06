4d70a5aac0e5f3bd2f5a0617b77932ff5cecba23
Bas de Vaan <bas.de.vaan@kabisa.nl>
2021-12-06 07:15:56 +0100

    refactor

--- day-06/lib/day_06.dart
+++ day-06/lib/day_06.dart
@@ -2,24 +2,28 @@ int amountOfFish(String fishList, int days) {
   var fish = fishList.split(',').map((f) => int.parse(f)).toList();
   var fishCount = <int, int>{};
 
+  // parse fish to map
   fish.forEach((element) {
     var newCount = fishCount[element] ?? 0;
     fishCount[element] = newCount + 1;
   });
 
+  // loop through days and map fish to day count
   for (var i = 0; i < days; i++) {
     var newFishCount = <int, int>{};
-    newFishCount[0] = fishCount[1] ?? 0;
-    newFishCount[1] = fishCount[2] ?? 0;
-    newFishCount[2] = fishCount[3] ?? 0;
-    newFishCount[3] = fishCount[4] ?? 0;
-    newFishCount[4] = fishCount[5] ?? 0;
-    newFishCount[5] = fishCount[6] ?? 0;
-    newFishCount[6] = (fishCount[7] ?? 0) + (fishCount[0] ?? 0);
-    newFishCount[7] = fishCount[8] ?? 0;
-    newFishCount[8] = fishCount[0] ?? 0;
+    for (var j = 0; j < 9; j++) {
+      newFishCount[j] = fishCount[j + 1] ?? 0;
+      if (j == 8) {
+        newFishCount[j] = fishCount[0] ?? 0;
+      }
+      if (j == 6) {
+        newFishCount[j] = (newFishCount[j] ?? 0) + (fishCount[0] ?? 0);
+      }
+    }
     fishCount = newFishCount;
   }
+
+  // count result
   var result = 0;
   fishCount.forEach((key, value) {
     result += value;
